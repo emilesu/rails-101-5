570eab91639bb3e0626f953324994e4a0f0ecf10
emilesu <emilesu@qq.com>
2017-02-16 20:03:36 +0800

    checkout owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -11,6 +11,10 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "你没有权限……"
+    end
   end
 
   def new
@@ -31,6 +35,10 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
 
+    if current_user != @group.user
+      redirect_to root_path, alert: "你没有权限……"
+    end
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "更新成功……"
     else
@@ -40,6 +48,11 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "你没有权限……"
+    end
+    
     @group.destroy
       redirect_to groups_path, notice: "删除成功……"
   end
